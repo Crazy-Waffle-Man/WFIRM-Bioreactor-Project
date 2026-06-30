ed08b7ff21ac63fffd031671bc40badfef6e7324
Crazy-Waffle-Man <benjamin.shifflet@gmail.com>
2026-06-30 12:02:01 -0400

    Finished adding widgets

--- esi-mp2/esi-mp2_gui-qt.py
+++ esi-mp2/esi-mp2_gui-qt.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QSlider, QTextEdit, QLabel, QLayout, QErrorMessage, QWidget
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QSlider, QTextEdit, QLabel, QLayout, QMessageBox, QWidget
 from PyQt6.QtCore import QSize, Qt
+from typing import Callable
 import serial, time
 
 class MainWindow(QMainWindow):
@@ -8,11 +9,61 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("ESI-MP2 control GUI")
         self.serial = AutoSerial(serial_port)
         self.motor = ESI_MP2(self.serial)
-    
-    def _setup_cmd_buttons(self):
+        self.setup_widgets()
+        self.show()
+
+    def setup_widgets(self):
+        #TODO: Layout
+        message_box = QMessageBox()
+
         stop_movement_button: QPushButton = QPushButton()
         stop_movement_button.setText("Stop Movement")
-        stop_movement_button.pressed.connect(self.motor.stop)
+        def stop():
+            message_box.setText(self.motor.stop())
+            message_box.show()
+        stop_movement_button.pressed.connect(stop)
+
+        direction_button: QPushButton = QPushButton()
+        direction_button.setCheckable(True)
+        get_direction: Callable = lambda: "ccw" if direction_button.isChecked() else "cw"
+        direction_button.setText(f"Currently turning {get_direction()}. Press to toggle.")
+        def toggle_direction():
+            if get_direction == "ccw":
+                self.motor.stop()
+                response = self.motor.turn_cw()
+            else:
+                self.motor.stop()
+                response = self.motor.turn_ccw()
+            message_box.setText(response)
+            message_box.show()
+        direction_button.toggled.connect(toggle_direction)
+        
+        status_button = QPushButton()
+        status_button.setText("Get status")
+        def get_status():
+            message_box.setText(self.motor.get_status())
+            message_box.show()
+        status_button.pressed.connect(get_status)
+
+        motor_speed_slider = QSlider()
+        motor_speed_slider.setRange(1, 3199)
+        mss_label = QLabel()
+        motor_speed_slider.valueChanged.connect(lambda: mss_label.setText(f"Current speed: {motor_speed_slider.value()}"))
+        
+        motor_speed_button = QPushButton()
+        motor_speed_button.setText("Set motor speed")
+        motor_speed_auto_button = QPushButton()
+        motor_speed_auto_button.setText("Motor speed auto-adjusts")
+        motor_speed_auto_button.setCheckable(True)
+        
+        def update_speed():
+            self.motor.set_motor_speed(motor_speed_slider.value())
+
+        def auto_adjust_speed():
+            if motor_speed_auto_button.isChecked(): update_speed()
+        motor_speed_slider.valueChanged.connect(auto_adjust_speed)
+
+
 
 
 
