943b4f71de764ff8fcdb5aacd5dc733284cd230a
Crazy-Waffle-Man <benjamin.shifflet@gmail.com>
2026-07-02 12:27:00 -0400

    Maybe it's here

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import live_graph
 import sensirion.i2c_data_getter
 
 import matplotlib.pyplot as plt
+from time import sleep
 
 graph = live_graph.LiveGraph(interval=100)
 plt.title("Flow rate vs time")
 plt.xlabel("Time (s)")
 plt.ylabel("Flow rate (mL/min)")
 # start live animation using the default random generator
-graph.start_animation(sensirion.i2c_data_getter.read_data_from_sensor())
+generator = sensirion.i2c_data_getter.read_data_from_sensor()
+sleep(5)
+graph.start_animation()
 plt.show()
