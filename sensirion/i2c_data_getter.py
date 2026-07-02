fc3271cc5a3cc79022f1b7b343ed7d6572f27144
Crazy-Waffle-Man <benjamin.shifflet@gmail.com>
2026-07-02 11:35:48 -0400

    Use the actual correct sensor type

--- sensirion/i2c_data_getter.py
+++ sensirion/i2c_data_getter.py
@@ -1,32 +1,30 @@
-from sensirion_uart_scc1.scc1_shdlc_device import Scc1ShdlcDevice
+import argparse
+
 from sensirion_shdlc_driver import ShdlcSerialPort, ShdlcConnection
-from sensirion_driver_adapters.i2c_adapter.i2c_channel import I2cChannel
-from sensirion_i2c_driver import CrcCalculator
-from sensirion_i2c_sf06_lf.device import Sf06LfDevice
-from sensirion_i2c_sf06_lf.commands import InvFlowScaleFactors
-import time
 
-# def ...(port, baudrate) -> Generator
-with ShdlcSerialPort(port="COM5", baudrate=115200) as port:
-    device = Scc1ShdlcDevice(ShdlcConnection(port))
-    device.set_sensor_type(3) # Flow sensor, thank goodness for docstrings.
-    channel = I2cChannel(device.get_i2c_transceiver(), slave_address=0x08, crc=CrcCalculator(8, 0x32, 0xff, 0x0))
+from sensirion_uart_scc1.drivers.scc1_slf3x import Scc1Slf3x
+from sensirion_uart_scc1.drivers.slf_common import get_flow_unit_label
+from sensirion_uart_scc1.scc1_shdlc_device import Scc1ShdlcDevice
 
-    sensor = Sf06LfDevice(channel)
+parser = argparse.ArgumentParser()
+parser.add_argument('--serial-port', '-p', default='COM5')
+args = parser.parse_args()
 
+with ShdlcSerialPort(port=args.serial_port, baudrate=115200) as port:
+    device = Scc1ShdlcDevice(ShdlcConnection(port), slave_address=0)
+    device.set_sensor_type(Scc1Slf3x.SENSOR_TYPE)
+    sensor = Scc1Slf3x(device)
+    print("serial_number:", sensor.serial_number)
+    print("product id:", sensor.product_id)
+    print("Flow;\tTemperature;\t Flag")
+    flow_scale, unit = sensor.get_flow_unit_and_scale()
+    sensor.start_continuous_measurement(interval_ms=2)
     try:
+        for _ in range(1000):
+            remaining, lost, data = sensor.read_extended_buffer()
+            print(f"Remaining bytes {remaining} and {lost}-sample lost")
+            print()
+            for flow, temperature, flag in data:
+                print(f'{flow / flow_scale} {get_flow_unit_label(unit)};\t{temperature / 200} C;\t {flag}')
+    finally:
         sensor.stop_continuous_measurement()
-        time.sleep(0.1)
-    except BaseException:
-        ... # TODO: error handling
-    (product_identifier, serial_number) = sensor.read_product_identifier()
-    print(f"{product_identifier}\n{serial_number}")
-    sensor.start_h2o_continuous_measurement()
-    for i in range(500):
-        try:
-            time.sleep(0.02)
-            (flow, temp, flags) = sensor.read_measurement_data(InvFlowScaleFactors.SLF3C_1300F)
-            print(flow, temp, flags)
-        except BaseException:
-            continue
-    sensor.stop_continuous_measurement()
