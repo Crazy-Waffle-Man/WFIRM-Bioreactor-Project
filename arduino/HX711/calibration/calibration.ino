e5b53ed92a53bbec3a276ce347cd48ec8c4cdd6a
Crazy-Waffle-Man <benjamin.shifflet@gmail.com>
2026-07-07 11:31:12 -0400

    Cleanup and calibration

--- arduino/HX711/calibration/calibration.ino
+++ arduino/HX711/calibration/calibration.ino
@@ -1,36 +1,38 @@
-#include "HX711.h"
+#include <HX711.h>
 
-#define DOUT 3
-#define CLK 2
+const int DOUT = A1;
+const int CLK = A0;
 
-HX711 scale
-float calibration_factor = 1
+HX711 scale;
+float calibration_factor = 8010;
+
+long zero_factor = 1;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("HX711 calibration sketch\nRemove all weight from scale\nAfter readings begin, place known weight on scale\n +/- to increase/decrease calibration factor");
   scale.begin(DOUT, CLK);
-  scale.set_scale();
+  scale.set_scale(calibration_factor);
   scale.tare();
-  long zero_factor = scale.read_average();
-  Serial.print(Zero factor: );
+  zero_factor = scale.read_average();
+  Serial.print("Zero factor: ");
   Serial.println(zero_factor);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  scale.set_scale(calibration_factor);
-
-  Serial.print("Reading: ");
-  Serial.print(scale.get_units(), 1);
-  Serial.print(" MPa, calibration_factor: ");
+  // scale.set_scale(calibration_factor);
+  Serial.print("Average of last 25 readings: ");
+  double reading = scale.get_units(25);
+  Serial.print(reading);
+  Serial.print(" mbar, calibration_factor: ");
   Serial.println(calibration_factor);
   if (Serial.available()) {
     char input = Serial.read();
     if (input == '+') {
       calibration_factor += 10;
     } else if (input == '-') {
-      calibration_factor -= 10
+      calibration_factor -= 10;
     }
   }
 }
