a769aa77f0b07457ef77d64992fe8ade0552d7ee
Crazy-Waffle-Man <benjamin.shifflet@gmail.com>
2026-07-09 11:21:59 -0400

    Add backend for json parser

---
+++ action_parser.py
@@ -0,0 +1,100 @@
+import json
+from typing import Callable
+from serial_helpers import ESI_MP2
+from goal_motor import *
+from arduino.HX711.bridge import get_data_from_arduino
+
+graph: LiveGraph
+motor: GoalMotor
+action_list = ActionList([])
+
+direction_is_cw: bool = False
+def adaptive_motor_speed(target: int | float, motor: ESI_MP2, value: int | float | Callable, tolerance: int | float):
+    global direction_is_cw
+    if isinstance(value, Callable):
+        value = value()
+    if value is None:
+        return
+    assert isinstance(value, (float, int))
+    if value < target - tolerance:
+        if not direction_is_cw:
+            motor.turn_cw()
+            direction_is_cw = True
+    elif value > target + tolerance:
+        if direction_is_cw:
+            motor.turn_ccw()
+            direction_is_cw = False
+        else:
+            motor.turn_ccw()
+            direction_is_cw = False
+
+def parse_action(action: dict) -> Action | None:
+    match action["action"]:
+        case "pressurize":
+            pressure = action["pressure"]
+            duration = action["duration"]
+            interval = action["check_every"]
+            repeats = duration / interval
+            _action = ActionTypes.Repeat(
+                ActionTypes.Actions(
+                    ActionTypes.CallUntilTarget(
+                        pressure, 
+                        adaptive_motor_speed,
+                        graph.get_latest_value,
+                        1,
+                        pressure,
+                        motor,
+                        graph.get_latest_value,
+                        1
+                    ),
+                    ActionTypes.Call(motor.stop),
+                    ActionTypes.Delay(interval)
+                ),
+                repeats
+            )
+            return _action
+        case "wait":
+            return ActionTypes.Delay(
+                    action["duration"]
+                )
+        case "repeat":
+            for i in range(action["repeats"]):
+                return action_list[-1]
+        case "multiple":
+            return ActionTypes.Actions(parse_action(act) for act in action["actions"])
+
+def get_action_list(file: str):
+    data = read(file)
+    for key in data.keys():
+        if key == "cycle":
+            cycle = data[key]
+            if isinstance(cycle, list):
+                for action in cycle:
+                    act = parse_action(action)
+                    if act:
+                        action_list.append_action(act)
+
+def read(file: str) -> dict:
+    with open(file, "r") as f:
+        result = json.load(f)
+        # print(f"Got dict by reading {file}:\n{dict}")
+        return result
+
+if __name__ == "__main__":
+    import argparse
+    from serial_helpers import AutoSerial
+    import matplotlib.pyplot as plt
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pressure_motor_port", "-p")
+    parser.add_argument("--arduino_port", "-a")
+    parser.add_argument("--file", "-f")
+    args = parser.parse_args()
+    motor = GoalMotor(AutoSerial(args.pressure_motor_port))
+    arduino_serial = AutoSerial(args.arduino_port)
+    graph = LiveGraph(50)
+    graph.start_animation(get_data_from_arduino(arduino_serial, "pressure"), interval=100)
+
+    get_action_list(args.file)
+    motor.actions = action_list
+    motor.go()
+    plt.show()
